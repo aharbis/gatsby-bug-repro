26b5eb87cdb5a276c982c5776011672baa424981
Aidan Harbison <aharbis@us.ibm.com>
2020-09-17 12:21:39 -0400

    Test MDX for comment build failure

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,10 @@
 import React from "react"
+import { Link } from "gatsby"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return <div>
+    Hello, world!
+    <br />
+    <Link to="/test">Go to test MDX</Link>
+  </div>
 }
